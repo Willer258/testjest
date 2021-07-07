ba709049050da65af1b1a1db6db87919745bdbd4
Willer <alainwill91@@gmail.com>
2021-07-07 23:44:09 +0000

    etude test

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    setupFilesAfterEnv: ["./jest.setup.js"],
+    moduleNameMapper: {
+      "^@components(.*)$": "<rootDir>/components$1",
+      "^@pages(.*)$": "<rootDir>/pages$1",
+      "^@hooks(.*)$": "<rootDir>/hooks$1",
+      "^.+\\.(css|less)$": "<rootDir>/CSSStub.js"
+    },
+    "testEnvironment": "jsdom",
+        
+      
+  };
